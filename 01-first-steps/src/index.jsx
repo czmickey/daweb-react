2bdae8639fcba80d4f7ef9c5258a314d45456649
Michal Oktábec <info@michaloktabec.cz>
2021-04-25 15:09:07 +0200

    Add 01-first-steps

---
+++ 01-first-steps/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.html';
+import './style.css'; 
+
+const name = 'Michal Oktábec';
+const cssIntro = 'intro';
+
+const App = (
+  <>
+    <header>
+      <h1>{name}</h1>
+    </header>
+    <main>
+      <p className={cssIntro}>Moje první React stránka</p>
+    </main>
+    <footer>Martin Podloucký</footer>
+  </>
+);
+
+render(
+  App,
+  document.querySelector('#app'),
+);
