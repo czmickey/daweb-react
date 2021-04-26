4bfda7ea0b8f064fb936a15a85aabec5dbe3da9f
Michal Oktábec <info@michaloktabec.cz>
2021-04-26 18:28:40 +0200

    Add 03-application

---
+++ 03-application/src/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = (props) => {
+    return (
+        <main>
+            <p>{props.content}</p>
+        </main>
+    );
+};
+
+export default Main;
---
+++ 03-application/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './index.html';
+import Main from './Main';
+
+const App = (
+  <>
+    <Header title="Titulek stránka" />
+    <Main content="Text z komponenty Main" />
+    <Footer author="Já" />
+  </>
+);
+
+render(
+  App,
+  document.querySelector('#app'),
+);
