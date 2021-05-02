c8a0a849255841ab8ba2eda087704e365d5f0726
Michal Oktábec <info@michaloktabec.cz>
2021-05-02 12:19:37 +0200

    Prettier ignore in Today component

--- 01-react-start/04-dates/src/components/Today.jsx
+++ 01-react-start/04-dates/src/components/Today.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Today = ({ day, month, year }) => (
-  <div className="date">
-    <span className="day">{day}</span>.<span className="month">{month}</span>.
-    <span className="year">{year}</span>
-  </div>
+  <>
+    {/* prettier-ignore */}
+    <div className="date">
+      <span className="day">{day}</span>.
+      <span className="month">{month}</span>.
+      <span className="year">{year}</span>
+    </div>
+  </>
 );
 
 export default Today;
